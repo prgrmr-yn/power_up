f2991df97ede66bb190254ab781a96d4e6f50a75
Stephen Choo <stephen.w.choo@gmail.com>
2022-11-26 16:36:24 +1100

    Made the basics for the powerbanks controller - not complete, but should be enough for any dependencies

--- app/controllers/powerbanks_controller.rb
+++ app/controllers/powerbanks_controller.rb
@@ -1,2 +1,29 @@
 class PowerbanksController < ApplicationController
+  def index
+    @powerbanks = Powerbank.all
+  end
+
+  def new
+    @powerbank = Powerbank.new
+  end
+
+  def show
+    @powerbank = Powerbank.find(params[:id])
+  end
+
+  def create
+    @powerbank = Powerbank.new(powerbank_params)
+    @powerbank.save
+    redirect_to powerbanks_path
+  end
+
+  def edit
+    @powerbank = Powerbank.find(params[:id])
+  end
+
+  def update
+    @powerbank = Powerbank.find(params[:id])
+    @powerbank.update(powerbank_params)
+    @powerbank.save
+  end
 end
