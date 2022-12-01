b77967e7ae4a8e94663bd80b6afa9eca00576508
Yatin Nagpal <nnega4428@gmail.com>
2022-12-01 19:15:02 +1100

    created a migration and seeded a instance through console

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_26_040921) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_074701) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_26_040921) do
     t.string "accessories"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["user_id"], name: "index_powerbanks_on_user_id"
   end
 
