fc25314d9912c393668cdf3b5f4040a05397bb3d
Yatin Nagpal <nnega4428@gmail.com>
2022-12-01 21:26:11 +1100

    fixed the after signup page and created navbar

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -297,6 +297,7 @@ Devise.setup do |config|
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
   # ==> Turbolinks configuration
+  config.navigational_formats = ['*/*', :html, :turbo_stream]
   # If your app is using Turbolinks, Turbolinks::Controller needs to be included to make redirection work correctly:
   #
   # ActiveSupport.on_load(:devise_failure_app) do
